9c4437e37192c42ccd3b0661e4c2fa52ee71b6b7
shivamverma1605 <shivam.verma_cs19@gla.ac.in>
2025-10-01 13:32:36 +0530

    updated services

---
+++ notification-service/src/main/java/com/codingshuttle/linkedin/posts_service/event/PostLikedEvent.java
@@ -0,0 +1,11 @@
+package com.codingshuttle.linkedin.posts_service.event;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+public class PostLikedEvent {
+    Long postId;
+    Long creatorId;
+    Long likedByUserId;
+}
---
+++ uploader-service/src/test/java/com/codingshuttle/linkedin/uploader_service/UploaderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.linkedin.uploader_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploaderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
